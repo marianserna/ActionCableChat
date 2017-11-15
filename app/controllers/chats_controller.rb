aa8eabe635f69eefbca99fa33d377b6f733a2dc2
Marian Serna <marianhalliday@gmail.com>
2017-11-14 20:56:14 -0500

    Chats controller actions

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,7 +1,33 @@
 # frozen_string_literal: true
 
 class ChatsController < ApplicationController
+  def new
+    @chat = Chat.new
+  end
+
+  def create
+    @chat = Chat.new(chat_params)
+    @chat.uuid = SecureRandom.uuid
+
+    if @chat.save
+      redirect_to chat_url(@chat.uuid)
+    else
+      render :new
+    end
+  end
+
   def show
-    @chat_props = {}
+    # id is actually uuid
+    @chat = Chat.find_by(uuid: params[:id])
+    @chat_props = {
+      chat: @chat,
+      messages: @chat.messages.order(created_at: :desc)
+    }
+  end
+
+  private
+
+  def chat_params
+    params.require(:chat).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  root to: 'chats#new'
   resources :chats, only: [:new, :create, :show]
 end
